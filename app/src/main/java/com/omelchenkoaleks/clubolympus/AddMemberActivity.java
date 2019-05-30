f874695e5f963db2b038353048ee9c466c77b2da
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-05-30 10:05:37 +0300

    FloatingActionButton and AddMemberActivity

---
+++ app/src/main/java/com/omelchenkoaleks/clubolympus/AddMemberActivity.java
@@ -0,0 +1,14 @@
+package com.omelchenkoaleks.clubolympus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddMemberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_member);
+    }
+}
