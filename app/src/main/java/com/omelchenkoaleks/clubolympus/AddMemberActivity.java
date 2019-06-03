276c53a71835082ba1d45cbe0f850388949a355b
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-06-03 10:09:54 +0300

    извлекаем данные при помощи переданного uri из db и помещаем их в соответствующие инпуты для редактирования ...

--- app/src/main/java/com/omelchenkoaleks/clubolympus/AddMemberActivity.java
+++ app/src/main/java/com/omelchenkoaleks/clubolympus/AddMemberActivity.java
@@ -3,6 +3,7 @@ package com.omelchenkoaleks.clubolympus;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -15,18 +16,26 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NavUtils;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
+import androidx.loader.content.Loader;
 
 import com.omelchenkoaleks.clubolympus.data.ClubOlympusContract.MemberEntry;
 
-public class AddMemberActivity extends AppCompatActivity {
+public class AddMemberActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<Cursor> {
+    private static final int EDIT_MEMBER_LADER = 111;
     private EditText mFirstNameEditText;
     private EditText mLastNameEditText;
     private EditText mSportEditText;
     private Spinner mGenderSpinner;
     private int mGender = 0;
     private ArrayAdapter mSpinnerAdapter;
+    private Uri mCurrentMemberUri;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -34,8 +43,8 @@ public class AddMemberActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_member);
 
         Intent intent = getIntent();
-        Uri currentMemberUri = intent.getData();
-        if (currentMemberUri == null) {
+        mCurrentMemberUri = intent.getData();
+        if (mCurrentMemberUri == null) {
             // если равен null - значит запуск произошел с помощью кнопки добавления
             setTitle("Add a Member");
         } else {
@@ -83,6 +92,9 @@ public class AddMemberActivity extends AppCompatActivity {
                 mGender = 0;
             }
         });
+
+        // нужно инициализировать loader
+        getSupportLoaderManager().initLoader(EDIT_MEMBER_LADER, null, this);
     }
 
     @Override
@@ -136,5 +148,66 @@ public class AddMemberActivity extends AppCompatActivity {
             Toast.makeText(this, "Data saved",
                     Toast.LENGTH_LONG).show();
         }
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        String[] projection = {
+                MemberEntry._ID,
+                MemberEntry.COLUMN_FIRST_NAME,
+                MemberEntry.COLUMN_LAST_NAME,
+                MemberEntry.COLUMN_GENDER,
+                MemberEntry.COLUMN_SPORT,
+        };
+
+        return new CursorLoader(this,
+                mCurrentMemberUri, // указываем uri нашей конктретной записи (который получаем)
+                projection,
+                null,
+                null,
+                null
+        );
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+        // теперь здесь нужно извлечь данные, которые были возвращены из предыдущего метода
+        if (data.moveToNext()) {
+            int firstNameColumnIndex = data.getColumnIndex(MemberEntry.COLUMN_FIRST_NAME);
+            int lastNameColumnIndex = data.getColumnIndex(MemberEntry.COLUMN_LAST_NAME);
+            int genderColumnIndex = data.getColumnIndex(MemberEntry.COLUMN_GENDER);
+            int sportColumnIndex = data.getColumnIndex(MemberEntry.COLUMN_SPORT);
+
+            /*
+                получив индексы столбцоа можно теперь извлекать данные по ним, а
+                потом присвоить эти данные соответствующим переменным
+             */
+            String firstName = data.getString(firstNameColumnIndex);
+            String lastName = data.getString(lastNameColumnIndex);
+            int gender = data.getInt(genderColumnIndex);
+            String sport = data.getString(sportColumnIndex);
+
+            mFirstNameEditText.setText(firstName);
+            mLastNameEditText.setText(lastName);
+            mSportEditText.setText(sport);
+
+            switch (gender) {
+                case MemberEntry.GENDER_MALE:
+                    mGenderSpinner.setSelection(1);
+                    break;
+                case MemberEntry.GENDER_FEMALE:
+                    mGenderSpinner.setSelection(2);
+                    break;
+                case MemberEntry.GENDER_UNKNOWN:
+                    mGenderSpinner.setSelection(0);
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
     }
 }
